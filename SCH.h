c45fb30feda267fa8a8fcbd9806432b403e56600
jsartist <unconfigured@null.spigotmc.org>
2019-09-17 16:25:30 +0900

    SCH.h

---
+++ SCH.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef __SCH_H__
+#define __SCH_H__
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <sstream>
+#include <conio.h>
+#pragma warning(disable:4996)
+using namespace std;
+
+
+
+
+#endif
